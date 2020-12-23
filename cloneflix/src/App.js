82ade7d19252fe30553fb32d850e40b28f9c73b2
thiagonmiziara <thiagonmiziara@gmail.com>
2020-12-22 21:33:01 -0300

    primeira parte do thiagoflix

---
+++ cloneflix/src/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import FeaturedMovie from "./components/FeaturedMovie/FeaturedMovie";
+import MovieRow from "./components/MovieRow/MovieRow";
+import Tmdb from "./Tmdb";
+
+const App = () => {
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //pegando a lista de filmes
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      //pegando o featured
+      let originals = list.filter((item) => item.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id,'tv');  
+      setFeaturedData(chosenInfo);
+      
+    };
+    loadAll();
+  }, []);
+  return (
+    <div className="page">
+      {featuredData && <FeaturedMovie item={featuredData} />}
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  );
+};
+
+export default App;
---
+++ cloneflix/src/components/FeaturedMovie/FeaturedMovie.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './FeaturedMovie.css';
+
+const FeaturedMovie = ({item}) => {
+  //parei no tempo 1.29:15
+  return (
+    <section className="fatured">
+      
+    </section>
+  )
+}
+
+export default FeaturedMovie
